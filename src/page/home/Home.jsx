f55446a863e0ef0e2fe4494808ee18e3769fc8d0
Jahid Howlader <jahidhowlader.info@gmail.com>
2023-05-18 10:12:58 +0600

    create a navbar

---
+++ src/page/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <section>
+                <img src="https://i.ibb.co/s3Y3SdR/Disney-Princess-Banner-00b-scaled.jpg" alt="" />
+            </section>
+        </>
+    );
+};
+
+export default Home;
